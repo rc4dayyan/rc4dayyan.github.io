61df90375bcce111c06e08cbe8469b4be34e1b77
rc4dayyan <daeytea@gmail.com>
2020-08-12 10:25:55 +0700

    new portfolio

---
+++ precache-manifest.0821a4c310613819e7b5bdc33ec74738.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68990156d7ed1dbe5fc76ec8a561c68c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770b0cd2af9150d9b1f6",
+    "url": "/static/css/2.6beaa794.chunk.css"
+  },
+  {
+    "revision": "115bf7bcc4ab9fb2ab0a",
+    "url": "/static/css/main.dcd4d6b8.chunk.css"
+  },
+  {
+    "revision": "770b0cd2af9150d9b1f6",
+    "url": "/static/js/2.557365e3.chunk.js"
+  },
+  {
+    "revision": "af1cb7975c7b067b0aa6b4aa93b911ae",
+    "url": "/static/js/2.557365e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115bf7bcc4ab9fb2ab0a",
+    "url": "/static/js/main.c26ea07a.chunk.js"
+  },
+  {
+    "revision": "166cf5f8ec0cbaf35406",
+    "url": "/static/js/runtime-main.d8445b1d.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "/static/media/navbg.74aaeeb9.jpg"
+  }
+]);
